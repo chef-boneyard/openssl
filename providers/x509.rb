ce593a7f6b36b12fd9f9f67cef66aa019bb4b104
Jesse Nelson <spheromak@gmail.com>
2013-05-29 14:32:47 -0700

    mark provider methods as private

--- providers/x509.rb
+++ providers/x509.rb
@@ -9,63 +9,6 @@ use_inline_resources
 
 attr_reader :key_file, :key, :cert, :ef
 
-def key_file
-  unless new_resource.key_file
-    path, file= ::File.split(new_resource.name)
-    filename  = ::File.basename(file, ::File.extname(file) )
-    new_resource.key_file  path + "/" + filename + ".key"
-  end
-  new_resource.key_file
-end
-
-def key
-  if ::File.exists? key_file 
-    @key ||= OpenSSL::PKey::RSA.new File.read(key_file), new_resource.key_pass
-  else
-    @key ||= OpenSSL::PKey::RSA.new(new_resource.key_length)
-  end
-  @key
-end
-
-def cert
-  @cert ||= OpenSSL::X509::Certificate.new
-end 
-
-def gen_cert
-  cert
-  cert.subject = cert.issuer = OpenSSL::X509::Name.parse(subject)
-  cert.not_before = Time.now
-  cert.not_after = Time.now + (new_resource.expire.to_i * 24 * 60 * 60)
-  cert.public_key = key.public_key
-  cert.serial = 0x0
-  cert.version = 2
-end
-
-def subject
-  @subject ||= "/C="  + new_resource.country + 
-               "/O="  + new_resource.org +
-               "/OU=" + new_resource.org_unit +
-               "/CN=" + new_resource.common_name
-end
-
-def extensions
-  [
-    ef.create_extension("basicConstraints","CA:TRUE", true),
-    ef.create_extension("subjectKeyIdentifier", "hash"),
-  ]
-end
-
-def create_keys
-  gen_cert
-  @ef ||= OpenSSL::X509::ExtensionFactory.new
-  ef.subject_certificate = cert
-  ef.issuer_certificate = cert
-  cert.extensions = extensions
-  cert.add_extension ef.create_extension("authorityKeyIdentifier",
-                                     "keyid:always,issuer:always")
-  cert.sign key, OpenSSL::Digest::SHA1.new
-end
-
 action :create  do 
   unless ::File.exists? new_resource.name 
     create_keys
@@ -88,3 +31,63 @@ action :create  do
 
   end
 end
+
+private 
+
+  def key_file
+    unless new_resource.key_file
+      path, file= ::File.split(new_resource.name)
+      filename  = ::File.basename(file, ::File.extname(file) )
+      new_resource.key_file  path + "/" + filename + ".key"
+    end
+    new_resource.key_file
+  end
+
+  def key
+    if ::File.exists? key_file 
+      @key ||= OpenSSL::PKey::RSA.new File.read(key_file), new_resource.key_pass
+    else
+      @key ||= OpenSSL::PKey::RSA.new(new_resource.key_length)
+    end
+    @key
+  end
+
+  def cert
+    @cert ||= OpenSSL::X509::Certificate.new
+  end 
+
+  def gen_cert
+    cert
+    cert.subject = cert.issuer = OpenSSL::X509::Name.parse(subject)
+    cert.not_before = Time.now
+    cert.not_after = Time.now + (new_resource.expire.to_i * 24 * 60 * 60)
+    cert.public_key = key.public_key
+    cert.serial = 0x0
+    cert.version = 2
+  end
+
+  def subject
+    @subject ||= "/C="  + new_resource.country + 
+                 "/O="  + new_resource.org +
+                 "/OU=" + new_resource.org_unit +
+                 "/CN=" + new_resource.common_name
+  end
+
+  def extensions
+    [
+      ef.create_extension("basicConstraints","CA:TRUE", true),
+      ef.create_extension("subjectKeyIdentifier", "hash"),
+    ]
+  end
+
+  def create_keys
+    gen_cert
+    @ef ||= OpenSSL::X509::ExtensionFactory.new
+    ef.subject_certificate = cert
+    ef.issuer_certificate = cert
+    cert.extensions = extensions
+    cert.add_extension ef.create_extension("authorityKeyIdentifier",
+                                       "keyid:always,issuer:always")
+    cert.sign key, OpenSSL::Digest::SHA1.new
+  end
+
