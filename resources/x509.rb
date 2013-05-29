fb2b819594c2a559c01e17fee1771045fd5628cf
Jesse Nelson <spheromak@gmail.com>
2013-05-29 14:05:04 -0700

    make org, org_unit, country, and common_name  required resource attributes

--- resources/x509.rb
+++ resources/x509.rb
@@ -7,10 +7,10 @@ attribute :owner,       :kind_of => String
 attribute :group,      :kind_of => String
 attribute :expire,     :kind_of => Fixnum
 attribute :mode,       :kind_of => String
-attribute :org,        :kind_of => String
-attribute :org_unit,   :kind_of => String
-attribute :country,    :kind_of => String
-attribute :common_name, :kind_of => String
+attribute :org,        :kind_of => String, :required => true
+attribute :org_unit,   :kind_of => String, :required => true
+attribute :country,    :kind_of => String, :required => true
+attribute :common_name, :kind_of => String, :required => true
 attribute :key_file,   :kind_of => String, :default => nil
 attribute :key_pass,   :kind_of => String, :default => nil
 attribute :key_length, :kind_of => Fixnum, :default => 2048
