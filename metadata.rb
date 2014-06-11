b75ab471185f108a2fadc5c43efb7e2ef057ab30
Sean OMeara <someara@opscode.com>
2014-06-11 16:13:03 -0400

    Version bump to 2.0.1 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Provides a library with a method for generating secure random passwords.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 recipe 'openssl', 'Empty, this cookbook provides a library, see README.md'
 
