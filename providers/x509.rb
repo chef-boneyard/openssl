c96b7e94e9ab87cbc6757bd0e6051d8ec40d27d7
Tim Smith <tsmith84@gmail.com>
2015-10-23 16:54:40 -0700

    Resolve rubocop warnings

--- providers/x509.rb
+++ providers/x509.rb
@@ -45,60 +45,59 @@ end
 
 protected
 
-  # rubocop:disable Metrics/AbcSize, Style/IndentationConsistency
-  def key_file
-    unless new_resource.key_file
-      path, file = ::File.split(new_resource.name)
-      filename = ::File.basename(file, ::File.extname(file))
-      new_resource.key_file path + '/' + filename + '.key'
-    end
-    new_resource.key_file
+def key_file
+  unless new_resource.key_file
+    path, file = ::File.split(new_resource.name)
+    filename = ::File.basename(file, ::File.extname(file))
+    new_resource.key_file path + '/' + filename + '.key'
   end
+  new_resource.key_file
+end
 
-  def key
-    @key ||= if key_file_valid?(key_file, new_resource.key_pass)
-               OpenSSL::PKey::RSA.new ::File.read(key_file), new_resource.key_pass
-             else
-               OpenSSL::PKey::RSA.new(new_resource.key_length)
-             end
-    @key
-  end
+def key
+  @key ||= if key_file_valid?(key_file, new_resource.key_pass)
+             OpenSSL::PKey::RSA.new ::File.read(key_file), new_resource.key_pass
+           else
+             OpenSSL::PKey::RSA.new(new_resource.key_length)
+           end
+  @key
+end
 
-  def cert
-    @cert ||= OpenSSL::X509::Certificate.new
-  end
+def cert
+  @cert ||= OpenSSL::X509::Certificate.new
+end
 
-  def gen_cert
-    cert
-    cert.subject = cert.issuer = OpenSSL::X509::Name.parse(subject)
-    cert.not_before = Time.now
-    cert.not_after = Time.now + (new_resource.expire.to_i * 24 * 60 * 60)
-    cert.public_key = key.public_key
-    cert.serial = 0x0
-    cert.version = 2
-  end
+def gen_cert
+  cert
+  cert.subject = cert.issuer = OpenSSL::X509::Name.parse(subject)
+  cert.not_before = Time.now
+  cert.not_after = Time.now + (new_resource.expire.to_i * 24 * 60 * 60)
+  cert.public_key = key.public_key
+  cert.serial = 0x0
+  cert.version = 2
+end
 
-  def subject
-    @subject ||= '/C=' + new_resource.country +
-                 '/O=' + new_resource.org +
-                 '/OU=' + new_resource.org_unit +
-                 '/CN=' + new_resource.common_name
-  end
+def subject
+  @subject ||= '/C=' + new_resource.country +
+               '/O=' + new_resource.org +
+               '/OU=' + new_resource.org_unit +
+               '/CN=' + new_resource.common_name
+end
 
-  def extensions
-    [
-      ef.create_extension('basicConstraints', 'CA:TRUE', true),
-      ef.create_extension('subjectKeyIdentifier', 'hash')
-    ]
-  end
+def extensions
+  [
+    ef.create_extension('basicConstraints', 'CA:TRUE', true),
+    ef.create_extension('subjectKeyIdentifier', 'hash')
+  ]
+end
 
-  def create_keys
-    gen_cert
-    @ef ||= OpenSSL::X509::ExtensionFactory.new
-    ef.subject_certificate = cert
-    ef.issuer_certificate = cert
-    cert.extensions = extensions
-    cert.add_extension ef.create_extension('authorityKeyIdentifier',
-                                           'keyid:always,issuer:always')
-    cert.sign key, OpenSSL::Digest::SHA256.new
-  end
+def create_keys
+  gen_cert
+  @ef ||= OpenSSL::X509::ExtensionFactory.new
+  ef.subject_certificate = cert
+  ef.issuer_certificate = cert
+  cert.extensions = extensions
+  cert.add_extension ef.create_extension('authorityKeyIdentifier',
+                                         'keyid:always,issuer:always')
+  cert.sign key, OpenSSL::Digest::SHA256.new
+end
