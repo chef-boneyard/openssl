9c1281cba9a3f4f6ff362aca56c1e9858cd1a8e8
Antek Baranski <antek.baranski@gmail.com>
2017-01-18 14:32:26 -0800

    Use proper ::File class and fix ^2 validation of dhparam key length
    Signed-off-by: Antek S. Baranski <antek.baranski@gmail.com>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -14,13 +14,13 @@ module OpenSSLCookbook
 
     # Validation helpers
     def key_length_valid?(number)
-      number >= 1024 && number & (number - 1).zero?
+      number >= 1024 && number & (number - 1) == 0
     end
 
     def dhparam_pem_valid?(dhparam_pem_path)
       # Check if the dhparam.pem file exists
       # Verify the dhparam.pem file contains a key
-      return false unless File.exist?(dhparam_pem_path)
+      return false unless ::File.exist?(dhparam_pem_path)
       dhparam = OpenSSL::PKey::DH.new File.read(dhparam_pem_path)
       dhparam.params_ok?
     end
@@ -28,7 +28,7 @@ module OpenSSLCookbook
     def key_file_valid?(key_file_path, key_password = nil)
       # Check if the key file exists
       # Verify the key file contains a private key
-      return false unless File.exist?(key_file_path)
+      return false unless ::File.exist?(key_file_path)
       key = OpenSSL::PKey::RSA.new File.read(key_file_path), key_password
       key.private?
     end
